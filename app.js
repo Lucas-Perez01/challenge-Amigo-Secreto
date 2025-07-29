fc69485c9764460e9ad2f643db6b9242f4f9d2a0
Lucas <paragithub01@gmail.com>
2025-07-28 22:20:23 -0300

    feat: Implementa función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 const amigos = [];
+let sorteado = false;
 
 const agregarAmigo = () => {
-  let texto = document.getElementById("amigo");
+  const texto = document.getElementById("amigo");
 
-  if (texto.value == "") {
+  if (texto.value === "") {
     alert("Por favor, inserte un nombre.");
     return;
+  }
+
+  amigos.push(texto.value);
+
+  if (sorteado) {
+    sorteado = false;
+    alert("Se reiniciara el sorteo.");
+    amigos.length = 0;
+    recorrerArrayDeAmigos();
   } else {
-    amigos.push(texto.value);
     recorrerArrayDeAmigos();
   }
 };
@@ -19,4 +28,19 @@ const recorrerArrayDeAmigos = () => {
   amigos.forEach((nombre) => {
     lista.innerHTML += `<li>${nombre}</li>`;
   });
+  return;
+};
+
+const sortearAmigo = () => {
+  const lista = document.getElementById("listaAmigos");
+
+  if (amigos.length === 0) {
+    alert("Debes ingresar los nombres de tus amigos");
+    return;
+  }
+
+  const indice = Math.floor(Math.random() * amigos.length);
+  lista.innerHTML = "";
+  lista.innerHTML += `<li>Amigo sorteado: ${amigos[indice]}</li>`;
+  sorteado = true;
 };
